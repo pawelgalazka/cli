d73329857a4437673c397bd64e2042ec1d9e503f
Pawel Galazka <pawel.galazka1986@gmail.com>
2017-10-22 12:07:13 +1100

    Additional test contexts

--- index.spec.js
+++ index.spec.js
@@ -8,8 +8,49 @@ describe('microcli', () => {
     callback = jest.fn()
   })
 
-  it('executes callback with params and options', () => {
-    microcli(['scriptname', '--foo=bar', '-a', 'abc', 'def'])(callback)
-    expect(callback).toHaveBeenCalledWith({a: true, foo: 'bar'}, 'abc', 'def')
+  describe('without annotations given', () => {
+    it('executes callback with params and options', () => {
+      microcli(['scriptname', '--foo=bar', '-a', 'abc', 'def'])(callback)
+      expect(callback).toHaveBeenCalledWith({a: true, foo: 'bar'}, 'abc', 'def')
+    })
+  })
+
+  describe('with annotations given', () => {
+    let annotations
+
+    describe('about params and options without descriptions', () => {
+      beforeEach(() => {
+        annotations = {
+          params: ['abc', 'def'],
+          options: ['a', 'foo']
+        }
+      })
+    })
+
+    describe('about params and options with descriptions', () => {
+      beforeEach(() => {
+        annotations = {
+          params: {
+            abc: 'description for abc param',
+            def: 'description for def param'
+          },
+          options: {
+            a: 'description for a option',
+            foo: 'description for foo option'
+          }
+        }
+      })
+
+      describe('and other extra', () => {
+        beforeEach(() => {
+          annotations['description'] = 'script description'
+          annotations['examples'] = 'examples content'
+        })
+
+        describe('with help formatting function', () => {
+
+        })
+      })
+    })
   })
 })
