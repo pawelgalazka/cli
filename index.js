d1873a0d4e60b55ed84fb78419062f2114b4f8eb
Pawel Galazka <pawel.galazka1986@gmail.com>
2017-10-22 22:18:54 +1100

    Linting fixes

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const microargs = require('microargs')
 const { get, difference } = require('lodash')
 
-function optionsToString(optionsKeys) {
+function optionsToString (optionsKeys) {
   return optionsKeys.map((optionName) => {
     return optionName.length === 1 ? `-${optionName}` : `--${optionName}`
   }).join(' ')
 }
 
-function printHelp(annotations) {
+function printHelp (annotations) {
 
 }
 
 module.exports = (argv, annotations = {}, printHelp = printHelp, logger = console) => {
   const { params, options } = microargs(argv.slice(1))
   const scriptName = argv[0]
-  const annotatedOptionsKeys = get(annotations, 'options') && Object.keys(annotations.options) || []
+  const annotatedOptionsKeys = (get(annotations, 'options') && Object.keys(annotations.options)) || []
   const optionsKeys = Object.keys(options)
   const illegalOptionsKeys = difference(optionsKeys, annotatedOptionsKeys)
 
