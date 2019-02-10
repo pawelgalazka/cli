9cad3c87512ed1b10727d6f1eac1c5325fd7a902
Paweł Gałązka <pawel.galazka1986@gmail.com>
2019-02-10 12:51:39 +1100

    Add printAllHelp function

--- src/help.ts
+++ src/help.ts
@@ -1,3 +1,4 @@
+import chalk from 'chalk'
 import { capitalize, forEach, isEmpty, omit, padEnd } from 'lodash'
 import path from 'path'
 
@@ -52,5 +53,36 @@ export function printHelp(
   forEach(extra, (value, key) => {
     logger.log(`\n${capitalize(key)}:\n`)
     logger.log(`${value}\n`)
+  })
+}
+
+export function printAllHelp(obj: any, logger: ILogger, namespace?: string) {
+  Object.keys(obj).forEach(key => {
+    const value = obj[key]
+    const nextNamespace = namespace ? `${namespace}:${key}` : key
+    const help = value.help
+
+    if (typeof value === 'function') {
+      // Add task name
+      const funcParams = help && help.params
+      const logArgs = [chalk.bold(nextNamespace)]
+
+      // Add task params
+      if (Array.isArray(funcParams) && funcParams.length) {
+        logArgs[0] += ` [${funcParams.join(' ')}]`
+      }
+
+      // Add description
+      if (help && (help.description || typeof help === 'string')) {
+        const description = help.description || help
+        logArgs[0] = padEnd(logArgs[0], 40) // format
+        logArgs.push('-', description.split('\n')[0])
+      }
+
+      // Log
+      logger.log(...logArgs)
+    } else if (typeof value === 'object') {
+      printAllHelp(value, logger, nextNamespace)
+    }
   })
 }
---
+++ tests/help.spec.ts
@@ -0,0 +1,109 @@
+/* tslint:disable:no-empty */
+import chalk from 'chalk'
+
+import { printAllHelp } from '../src/help'
+
+describe('printAllHelp()', () => {
+  let obj: any
+  let logger: any
+  let mockLogger: any
+
+  beforeEach(() => {
+    obj = {
+      a: () => {},
+      b: () => {}
+    }
+
+    mockLogger = jest.fn()
+    logger = {
+      error: (...args: any[]) => {
+        mockLogger('error', ...args)
+      },
+      log: (...args: any[]) => {
+        mockLogger('log', ...args)
+      },
+      title: (...args: any[]) => {
+        mockLogger('title', ...args)
+      },
+      warning: (...args: any[]) => {
+        mockLogger('warning', ...args)
+      }
+    }
+  })
+
+  it('should log list of methods', () => {
+    printAllHelp(obj, logger)
+    expect(mockLogger.mock.calls).toEqual([
+      ['log', chalk.bold('a')],
+      ['log', chalk.bold('b')]
+    ])
+  })
+
+  it('should log method descriptions', () => {
+    obj.b = (arg1: any, arg2: any) => {}
+    obj.a.help = 'Description for method a'
+    obj.b.help = 'Description for method b'
+    printAllHelp(obj, logger)
+    expect(mockLogger.mock.calls).toEqual([
+      [
+        'log',
+        chalk.bold('a') + '                              ',
+        '-',
+        'Description for method a'
+      ],
+      [
+        'log',
+        chalk.bold('b') + '                              ',
+        '-',
+        'Description for method b'
+      ]
+    ])
+  })
+
+  it('should log only first line of method descriptions', () => {
+    obj.a.help = 'Description for method a\nsecond line\nthird line'
+    obj.b.help = 'Description for method b'
+    printAllHelp(obj, logger)
+    expect(mockLogger.mock.calls).toEqual([
+      [
+        'log',
+        chalk.bold('a') + '                              ',
+        '-',
+        'Description for method a'
+      ],
+      [
+        'log',
+        chalk.bold('b') + '                              ',
+        '-',
+        'Description for method b'
+      ]
+    ])
+  })
+
+  it('should log list of name spaced / nested methods', () => {
+    obj.c = {
+      d: () => {},
+      e: {
+        f: () => {},
+        g: () => {}
+      }
+    }
+
+    obj.c.help = 'Description for namespace c'
+    obj.c.e.f.help = 'Description for method f'
+
+    printAllHelp(obj, logger)
+    expect(mockLogger.mock.calls).toEqual([
+      ['log', chalk.bold('a')],
+      ['log', chalk.bold('b')],
+      ['log', chalk.bold('c:d')],
+      [
+        'log',
+        chalk.bold('c:e:f') + '                          ',
+        '-',
+        'Description for method f'
+      ],
+      ['log', chalk.bold('c:e:g')]
+    ])
+  })
+})
