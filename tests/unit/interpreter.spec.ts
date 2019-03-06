7e41787004905a303a1cd92d26d8e0d6b23965ea
Paweł Gałązka <pawel.galazka1986@gmail.com>
2019-03-06 19:43:41 +1100

    Provide additional test context

--- tests/unit/interpreter.spec.ts
+++ tests/unit/interpreter.spec.ts
@@ -16,62 +16,64 @@ describe('interpreter', () => {
       }
     })
 
-    it('calls command', () => {
-      interpret({ options: {}, params: ['a'], commandsModule, namespace: '' })
-      expect(commandsModule.a).toHaveBeenCalledTimes(1)
-      expect(commandsModule.a).toHaveBeenCalledWith({})
-    })
-
-    it('calls default command', () => {
-      interpret({ options: {}, params: [], commandsModule, namespace: '' })
-      expect(commandsModule.default).toHaveBeenCalledTimes(1)
-      expect(commandsModule.default).toHaveBeenCalledWith({})
-    })
-
-    it('calls command with params', () => {
-      interpret({
-        commandsModule,
-        namespace: '',
-        options: {},
-        params: ['a', 'arg1', 'arg2']
+    describe('first module level', () => {
+      it('calls command', () => {
+        interpret({ options: {}, params: ['a'], commandsModule, namespace: '' })
+        expect(commandsModule.a).toHaveBeenCalledTimes(1)
+        expect(commandsModule.a).toHaveBeenCalledWith({})
       })
-      expect(commandsModule.a).toHaveBeenCalledTimes(1)
-      expect(commandsModule.a).toHaveBeenCalledWith({}, 'arg1', 'arg2')
-    })
 
-    it('calls default command with params', () => {
-      interpret({
-        commandsModule,
-        namespace: '',
-        options: {},
-        params: ['arg1', 'arg2']
+      it('calls default command', () => {
+        interpret({ options: {}, params: [], commandsModule, namespace: '' })
+        expect(commandsModule.default).toHaveBeenCalledTimes(1)
+        expect(commandsModule.default).toHaveBeenCalledWith({})
       })
-      expect(commandsModule.default).toHaveBeenCalledTimes(1)
-      expect(commandsModule.default).toHaveBeenCalledWith({}, 'arg1', 'arg2')
-    })
 
-    it('calls command with options', () => {
-      const options = { op1: 'o1', op2: 'o2' }
-      interpret({
-        commandsModule,
-        namespace: '',
-        options,
-        params: ['a']
+      it('calls command with params', () => {
+        interpret({
+          commandsModule,
+          namespace: '',
+          options: {},
+          params: ['a', 'arg1', 'arg2']
+        })
+        expect(commandsModule.a).toHaveBeenCalledTimes(1)
+        expect(commandsModule.a).toHaveBeenCalledWith({}, 'arg1', 'arg2')
       })
-      expect(commandsModule.a).toHaveBeenCalledTimes(1)
-      expect(commandsModule.a).toHaveBeenCalledWith(options)
-    })
 
-    it('calls default command with options', () => {
-      const options = { op1: 'o1', op2: 'o2' }
-      interpret({
-        commandsModule,
-        namespace: '',
-        options,
-        params: []
+      it('calls default command with params', () => {
+        interpret({
+          commandsModule,
+          namespace: '',
+          options: {},
+          params: ['arg1', 'arg2']
+        })
+        expect(commandsModule.default).toHaveBeenCalledTimes(1)
+        expect(commandsModule.default).toHaveBeenCalledWith({}, 'arg1', 'arg2')
       })
-      expect(commandsModule.default).toHaveBeenCalledTimes(1)
-      expect(commandsModule.default).toHaveBeenCalledWith(options)
+
+      it('calls command with options', () => {
+        const options = { op1: 'o1', op2: 'o2' }
+        interpret({
+          commandsModule,
+          namespace: '',
+          options,
+          params: ['a']
+        })
+        expect(commandsModule.a).toHaveBeenCalledTimes(1)
+        expect(commandsModule.a).toHaveBeenCalledWith(options)
+      })
+
+      it('calls default command with options', () => {
+        const options = { op1: 'o1', op2: 'o2' }
+        interpret({
+          commandsModule,
+          namespace: '',
+          options,
+          params: []
+        })
+        expect(commandsModule.default).toHaveBeenCalledTimes(1)
+        expect(commandsModule.default).toHaveBeenCalledWith(options)
+      })
     })
   })
 
