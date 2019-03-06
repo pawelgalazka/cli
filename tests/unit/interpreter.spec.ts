1513da0ad2b6c52eca0937a14476c8287d20a094
Paweł Gałązka <pawel.galazka1986@gmail.com>
2019-03-06 19:26:16 +1100

    interpreter: some basic call tests

--- tests/unit/interpreter.spec.ts
+++ tests/unit/interpreter.spec.ts
@@ -1,3 +1,5 @@
+import { interpret } from '../../src/interpreter'
+
 describe('interpreter', () => {
   let commandsModule: any
   describe('when commandsModule object given', () => {
@@ -12,6 +14,35 @@ describe('interpreter', () => {
         },
         default: jest.fn()
       }
+    })
+
+    it('calls command', () => {
+      interpret({ options: {}, params: ['a'], commandsModule, namespace: '' })
+      expect(commandsModule.a).toHaveBeenCalledTimes(1)
+      expect(commandsModule.a).toHaveBeenCalledWith({})
+    })
+
+    it('calls command with params', () => {
+      interpret({
+        commandsModule,
+        namespace: '',
+        options: {},
+        params: ['a', 'arg1', 'arg2']
+      })
+      expect(commandsModule.a).toHaveBeenCalledTimes(1)
+      expect(commandsModule.a).toHaveBeenCalledWith({}, 'arg1', 'arg2')
+    })
+
+    it('calls command with options', () => {
+      const options = { op1: 'o1', op2: 'o2' }
+      interpret({
+        commandsModule,
+        namespace: '',
+        options,
+        params: ['a']
+      })
+      expect(commandsModule.a).toHaveBeenCalledTimes(1)
+      expect(commandsModule.a).toHaveBeenCalledWith(options)
     })
   })
 
