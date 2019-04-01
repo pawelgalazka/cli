413d826cb4a0acce6a599276ab34b076671529e5
Paweł Gałązka <pawel.galazka1986@gmail.com>
2019-04-01 20:06:35 +1100

    Add more tests for interpreter

--- tests/unit/interpreter.spec.ts
+++ tests/unit/interpreter.spec.ts
@@ -3,6 +3,7 @@ import { interpret } from '../../src/interpreter'
 
 describe('interpreter', () => {
   let commandsModule: any
+
   describe('when commandsModule object given', () => {
     beforeEach(() => {
       commandsModule = {
@@ -11,13 +12,15 @@ describe('interpreter', () => {
           c: jest.fn(),
           d: {
             e: jest.fn()
-          }
+          },
+          default: jest.fn()
         },
+        f: {},
         default: jest.fn()
       }
     })
 
-    describe('and no namespace in task name', () => {
+    describe('with no namespace in task name', () => {
       it('calls command', () => {
         interpret({ options: {}, params: ['a'], commandsModule })
         expect(commandsModule.a).toHaveBeenCalledTimes(1)
@@ -79,6 +82,52 @@ describe('interpreter', () => {
             commandsModule,
             options: {},
             params: ['arg1', 'arg2']
+          })
+        }).toThrow(CLICommandNotFound)
+      })
+    })
+
+    describe('with namespace in task name', () => {
+      it('calls command from name space', () => {
+        interpret({ options: {}, params: ['b:c'], commandsModule })
+        expect(commandsModule.b.c).toHaveBeenCalledTimes(1)
+        expect(commandsModule.b.c).toHaveBeenCalledWith({})
+      })
+
+      it('calls default command from name space', () => {
+        interpret({ options: {}, params: ['b'], commandsModule })
+        expect(commandsModule.b.default).toHaveBeenCalledTimes(1)
+        expect(commandsModule.b.default).toHaveBeenCalledWith({})
+      })
+
+      it('calls command from name space with options', () => {
+        const options = { op1: 'o1', op2: 'o2' }
+        interpret({
+          commandsModule,
+          options,
+          params: ['b:c']
+        })
+        expect(commandsModule.b.c).toHaveBeenCalledTimes(1)
+        expect(commandsModule.b.c).toHaveBeenCalledWith(options)
+      })
+
+      it('calls default command from name space with options', () => {
+        const options = { op1: 'o1', op2: 'o2' }
+        interpret({
+          commandsModule,
+          options,
+          params: ['b']
+        })
+        expect(commandsModule.b.default).toHaveBeenCalledTimes(1)
+        expect(commandsModule.b.default).toHaveBeenCalledWith(options)
+      })
+
+      it('throws error if no default command and command from name space not found', () => {
+        expect(() => {
+          interpret({
+            commandsModule,
+            options: {},
+            params: ['f', 'arg1', 'arg2']
           })
         }).toThrow(CLICommandNotFound)
       })
