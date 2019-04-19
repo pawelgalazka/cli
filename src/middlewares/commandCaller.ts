25146bb57931ea77240fdd9d97675c872f3a2ec0
Paweł Gałązka <pawel.galazka1986@gmail.com>
2019-04-19 18:53:26 +1000

    commandCaller: throw error if command not found

--- src/middlewares/commandCaller.ts
+++ src/middlewares/commandCaller.ts
@@ -1,7 +1,10 @@
-import { Middleware } from '../index'
+import { CLIError, Middleware } from '../index'
 
 export const commandCaller: Middleware = next => args => {
   const { command, options, params, reject } = args
+  if (!command) {
+    throw new CLIError('Command not found')
+  }
   Promise.resolve(command(options, ...params))
     .then(() => {
       next(args)
